36f077ee724787cd10e2c62baf900fb2cb45ef95
FZHARON <popoformac@gmail.com>
2024-08-08 00:46:34 +0900

    남해병원 데이터 추가 및 assets 파일 정리

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,8 @@ class _MapPageState extends State<MapPage> {
   List<Map<String, dynamic>> _properties1 = [];
   List<MapLatLng> _dataPoints2 = [];
   List<Map<String, dynamic>> _properties2 = [];
+  List<MapLatLng> _dataPoints3 = [];
+  List<Map<String, dynamic>> _properties3 = [];
   MapLatLng? _currentLocation;
   late MapZoomPanBehavior _zoomPanBehavior;
   bool _isLoading = true;
@@ -46,8 +48,8 @@ class _MapPageState extends State<MapPage> {
   void initState() {
     super.initState();
     _zoomPanBehavior = MapZoomPanBehavior(
-      focalLatLng: const MapLatLng(36.5, 127.5), // 대한민국 중심 좌표 설정
-      zoomLevel: 7, // 적절한 줌 레벨 설정
+      focalLatLng: const MapLatLng(36.5, 127.5),
+      zoomLevel: 7,
       enableDoubleTapZooming: true,
       enablePanning: true,
       enablePinching: true,
@@ -61,11 +63,13 @@ class _MapPageState extends State<MapPage> {
     await Future.wait([
       _loadGeoJson1(),
       _loadGeoJson2(),
+      _loadGeoJson3(),
     ]);
     await _getCurrentLocation();
     _allSuggestions = [
       ..._properties1.map((e) => e['NAME'] ?? 'No Name'),
       ..._properties2.map((e) => e['DEPART_NM'] ?? 'No Name'),
+      ..._properties3.map((e) => e['업소명'] ?? 'No Name'),
     ];
     setState(() {
       _isLoading = false;
@@ -74,7 +78,7 @@ class _MapPageState extends State<MapPage> {
 
   Future<void> _loadGeoJson1() async {
     final String response =
-        await rootBundle.loadString('assets/seaPolice.geojson');
+        await rootBundle.loadString('assets/datas/seaPolice.geojson');
     final data = json.decode(response);
     final List<MapLatLng> points = [];
     final List<Map<String, dynamic>> properties = [];
@@ -82,7 +86,10 @@ class _MapPageState extends State<MapPage> {
     for (var feature in data['features']) {
       if (feature['geometry']['type'] == 'Point') {
         var coordinates = feature['geometry']['coordinates'];
-        points.add(MapLatLng(coordinates[1], coordinates[0]));
+        points.add(MapLatLng(
+          coordinates[1].toDouble(),
+          coordinates[0].toDouble(),
+        ));
         properties.add(feature['properties']);
       }
     }
@@ -94,7 +101,8 @@ class _MapPageState extends State<MapPage> {
   }
 
   Future<void> _loadGeoJson2() async {
-    final String response = await rootBundle.loadString('assets/ship.geojson');
+    final String response =
+        await rootBundle.loadString('assets/datas/ship.geojson');
     final data = json.decode(response);
     final List<MapLatLng> points = [];
     final List<Map<String, dynamic>> properties = [];
@@ -102,7 +110,10 @@ class _MapPageState extends State<MapPage> {
     for (var feature in data['features']) {
       if (feature['geometry']['type'] == 'Point') {
         var coordinates = feature['geometry']['coordinates'];
-        points.add(MapLatLng(coordinates[1], coordinates[0]));
+        points.add(MapLatLng(
+          coordinates[1].toDouble(),
+          coordinates[0].toDouble(),
+        ));
         properties.add(feature['properties']);
       }
     }
@@ -110,6 +121,30 @@ class _MapPageState extends State<MapPage> {
     setState(() {
       _dataPoints2 = points;
       _properties2 = properties;
+    });
+  }
+
+  Future<void> _loadGeoJson3() async {
+    final String response =
+        await rootBundle.loadString('assets/datas/namhaHData.geojson');
+    final data = json.decode(response);
+    final List<MapLatLng> points = [];
+    final List<Map<String, dynamic>> properties = [];
+
+    for (var feature in data['features']) {
+      if (feature['geometry']['type'] == 'Point') {
+        var coordinates = feature['geometry']['coordinates'];
+        points.add(MapLatLng(
+          coordinates[1].toDouble(),
+          coordinates[0].toDouble(),
+        ));
+        properties.add(feature['properties']);
+      }
+    }
+
+    setState(() {
+      _dataPoints3 = points;
+      _properties3 = properties;
     });
   }
 
@@ -141,7 +176,7 @@ class _MapPageState extends State<MapPage> {
   }
 
   void _searchLocation(String query) {
-    FocusScope.of(context).unfocus(); // 가상 키보드 내리기
+    FocusScope.of(context).unfocus();
     for (int i = 0; i < _properties1.length; i++) {
       if (_properties1[i]['NAME'] == query) {
         _zoomPanBehavior.focalLatLng = _dataPoints1[i];
@@ -153,6 +188,14 @@ class _MapPageState extends State<MapPage> {
     for (int i = 0; i < _properties2.length; i++) {
       if (_properties2[i]['DEPART_NM'] == query) {
         _zoomPanBehavior.focalLatLng = _dataPoints2[i];
+        _zoomPanBehavior.zoomLevel = 15;
+        return;
+      }
+    }
+
+    for (int i = 0; i < _properties3.length; i++) {
+      if (_properties3[i]['업소명'] == query) {
+        _zoomPanBehavior.focalLatLng = _dataPoints3[i];
         _zoomPanBehavior.zoomLevel = 15;
         return;
       }
@@ -219,8 +262,7 @@ class _MapPageState extends State<MapPage> {
                         SfMaps(
                           layers: [
                             MapTileLayer(
-                              initialFocalLatLng:
-                                  const MapLatLng(36.5, 127.5), // 대한민국 중심 좌표 설정
+                              initialFocalLatLng: const MapLatLng(36.5, 127.5),
                               initialZoomLevel: 7,
                               urlTemplate:
                                   'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -228,18 +270,22 @@ class _MapPageState extends State<MapPage> {
                               initialMarkersCount: (_currentLocation != null
                                   ? _dataPoints1.length +
                                       _dataPoints2.length +
+                                      _dataPoints3.length +
                                       1
-                                  : _dataPoints1.length + _dataPoints2.length),
+                                  : _dataPoints1.length +
+                                      _dataPoints2.length +
+                                      _dataPoints3.length),
                               markerBuilder: (BuildContext context, int index) {
                                 if (_currentLocation != null &&
                                     index ==
                                         _dataPoints1.length +
-                                            _dataPoints2.length) {
+                                            _dataPoints2.length +
+                                            _dataPoints3.length) {
                                   return MapMarker(
                                     latitude: _currentLocation!.latitude,
                                     longitude: _currentLocation!.longitude,
                                     child: Image.asset(
-                                      'assets/yourlocation.png', // 여기에 사용하고자 하는 아이콘의 경로를 넣으세요.
+                                      'assets/icons/yourlocation.png',
                                       width: 30,
                                       height: 30,
                                     ),
@@ -254,13 +300,14 @@ class _MapPageState extends State<MapPage> {
                                             context, _properties1[index], true);
                                       },
                                       child: Image.asset(
-                                        'assets/policeicon2.png', // 여기에 사용하고자 하는 아이콘의 경로를 넣으세요.
+                                        'assets/icons/policeicon2.png',
                                         width: 24,
                                         height: 24,
                                       ),
                                     ),
                                   );
-                                } else {
+                                } else if (index <
+                                    _dataPoints1.length + _dataPoints2.length) {
                                   int adjustedIndex =
                                       index - _dataPoints1.length;
                                   return MapMarker(
@@ -274,7 +321,28 @@ class _MapPageState extends State<MapPage> {
                                             _properties2[adjustedIndex], false);
                                       },
                                       child: Image.asset(
-                                        'assets/shipicon2.png', // 여기에 사용하고자 하는 아이콘의 경로를 넣으세요.
+                                        'assets/icons/shipicon2.png',
+                                        width: 24,
+                                        height: 24,
+                                      ),
+                                    ),
+                                  );
+                                } else {
+                                  int adjustedIndex = index -
+                                      _dataPoints1.length -
+                                      _dataPoints2.length;
+                                  return MapMarker(
+                                    latitude:
+                                        _dataPoints3[adjustedIndex].latitude,
+                                    longitude:
+                                        _dataPoints3[adjustedIndex].longitude,
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        _showMarkerInfo(context,
+                                            _properties3[adjustedIndex], false);
+                                      },
+                                      child: Image.asset(
+                                        'assets/icons/namhaHicon.png',
                                         width: 24,
                                         height: 24,
                                       ),
@@ -302,6 +370,10 @@ class _MapPageState extends State<MapPage> {
                                   color: Colors.green,
                                   text: '항구 데이터',
                                 ),
+                                LegendItem(
+                                  color: Colors.blue,
+                                  text: '남해병원 데이터',
+                                ),
                               ],
                             ),
                           ),
@@ -322,7 +394,10 @@ class _MapPageState extends State<MapPage> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text(
-            properties['NAME'] ?? properties['DEPART_NM'] ?? 'Marker Info',
+            properties['NAME'] ??
+                properties['DEPART_NM'] ??
+                properties['업소명'] ??
+                'Marker Info',
           ),
           content: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -330,8 +405,12 @@ class _MapPageState extends State<MapPage> {
             children: properties.entries.where((entry) {
               if (isSeaPolice) {
                 return entry.key == 'RNADRES';
-              } else {
+              } else if (properties.containsKey('DEPART_NM')) {
                 return entry.key == 'SHIP_CNT' || entry.key == 'DEPART_NM';
+              } else {
+                return entry.key == '전화번호' ||
+                    entry.key == '주소' ||
+                    entry.key == '업소명';
               }
             }).map((entry) {
               return Text('${entry.key}: ${entry.value}');
@@ -342,7 +421,7 @@ class _MapPageState extends State<MapPage> {
               child: const Text('Close'),
               onPressed: () {
                 Navigator.of(context).pop();
-                FocusScope.of(context).unfocus(); // 가상 키보드 내리기
+                FocusScope.of(context).unfocus();
               },
             ),
           ],
